0a4a0104481f7a00c91785cc2c6169f0e6958889
kooroosh1363 <peymanraad1363@gmail.com>
2023-09-23 00:12:26 +0400

    add css codes

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Swiper, SwiperSlider} from "swiper/react";
+// import './App.css';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import {EffectCoverflow,pagination,Navigation} from 'swiper';
+
+import slide_image_1 from "./assets/images/36W-black.jpg";
+import slide_image_2 from "./assets/images/36W-green.jpg";
+import slide_image_3 from "./assets/images/36W-red.jpg";
+import slide_image_4 from "./assets/images/36W-white.jpg";
+import slide_image_5 from "./assets/images/46V-sand.jpg";
+import slide_image_6 from "./assets/images/48V-grey-white.jpg";
+function App() {
+  
+
+  return (
+    <div className="container">
+      <h1 className="heading">hat store</h1>
+    </div>
+  );
+}
+
+export default App;
