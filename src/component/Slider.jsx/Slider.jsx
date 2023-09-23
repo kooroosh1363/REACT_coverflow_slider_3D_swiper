bef727e9952ec745440f844e315a982f5bf78db2
kooroosh1363 <peymanraad1363@gmail.com>
2023-09-23 13:48:33 +0400

    add npm icon and bootstrap

---
+++ src/component/Slider.jsx/Slider.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+export const Slider = () => {
+    return (
+        <div className="container">
+            <h1>hello Slider</h1><br />
+        </div>
+    );
+};
+
+
